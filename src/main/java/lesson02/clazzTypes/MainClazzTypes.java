a26d4cf8d974591c5c806b2e1fc21406831d141c
Serg <Serg@PK>
2018-12-25 12:53:49 +0200

    Add lesson03 examples

--- src/main/java/lesson02/clazzTypes/MainClazzTypes.java
+++ src/main/java/lesson02/clazzTypes/MainClazzTypes.java
@@ -9,6 +9,7 @@ import java.util.List;
 public class MainClazzTypes {
 	public static void main(String[] args) {
 		List<Animal> animals = new ArrayList<>();
+		String nameNestedClass= "Cat";
 		Animal animal1 = new Animal() {
 
 			@Override
@@ -18,17 +19,26 @@ public class MainClazzTypes {
 
 			@Override
 			public String name() {
-				return "Cat";
+				return nameNestedClass;
 			}
 		};
+		//You can not change it 
+		//nameNestedClass="";
 		animals.add(animal1);
 
 		Animal animal2 = new AnimalExample.Pig();
 		animals.add(animal2);
 
+		//Haven't access directly, without instance of parent class 
 		// Animal animal3 = new AnimalExample.Dog();
 		// animals.add(animal3);
+		
+		//Cannot create static nested class from instance of parent class
+		//AnimalExample animalExample =new AnimalExample(); 
+		//animalExample.new Dog();
+		//animalExample.new Pig();
 
+		
 		Animal animal4 = new AnimalExample().new Dog();
 		animals.add(animal4);
 
