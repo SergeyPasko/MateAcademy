ff92ca766f0d8bdef83441c08cb0a7bb4ebc8aea
spasko <spasko@smc-pasko.innovecs.local>
2019-01-10 14:55:30 +0200

    lesson07 copyOnWrite and blocking queue

--- src/main/java/lesson07/multitheading/createThread/MainTheadCreator.java
+++ src/main/java/lesson07/multitheading/createThread/MainTheadCreator.java
@@ -1,5 +1,7 @@
 package lesson07.multitheading.createThread;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author spasko
  */
@@ -9,11 +11,44 @@ public class MainTheadCreator {
 			String threadName = Thread.currentThread().getName();
 			System.out.println("Hello " + threadName);
 		};
-		task.run();
 
 		Thread thread = new Thread(task);
 		thread.start();
 
 		System.out.println("Done!");
+
+		IntCounter count = new IntCounter(0);
+		Runnable task2 = () -> {
+			for (int i = 0; i < 5000; i++) {
+				count.increment();
+			}
+		};
+
+		(new Thread(task2)).start();
+		(new Thread(task2)).start();
+
+		try {
+			TimeUnit.SECONDS.sleep(3);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Result count:" + count.getCount());
+	}
+
+	private static class IntCounter {
+		private int count;
+
+		public IntCounter(int count) {
+			this.count = count;
+		}
+
+		public int getCount() {
+			return count;
+		}
+
+		public void increment() {
+			count++;
+		}
+
 	}
 }
--- src/main/java/lesson07/multitheading/singleton/S1EagerInitializedSingleton.java
+++ src/main/java/lesson07/multitheading/singleton/S1EagerInitializedSingleton.java
@@ -3,13 +3,12 @@ package lesson07.multitheading.singleton;
 /**
  * @author spasko
  */
-public class S1EagerInitializedSingleton extends Animal {
+public class S1EagerInitializedSingleton {
 
 	private static final S1EagerInitializedSingleton instance = new S1EagerInitializedSingleton();
 
 	// private constructor to avoid client applications to use constructor
 	private S1EagerInitializedSingleton() {		
-		super("S1EagerInitializedSingleton " + System.currentTimeMillis());
 	}
 
 	public static S1EagerInitializedSingleton getInstance() {
